a85982ac7f5725e2dc7dd975a62ed2616c529b70
swgoof <github@swgoof.com>
2011-01-15 14:49:28 -0600

    add license and minor updates

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,6 +18,9 @@ import play.modules.morphia.utils.MorphiaFixtures;
 import play.mvc.Controller;
 import play.test.Fixtures;
 
+/**
+ *
+ */
 public class Application extends Controller {
 
     public static void index() {
